e9c2203c5106ce1afac62bc3617ab3244a6a3bbe
Selam90 <selamnega0@gmail.com>
2022-10-14 07:05:02 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,2 +1,21 @@
+#include <stdio.h>
+/*
+ * main
+ * return
+ * */
+int main(void)
+{
+	char c;
+
+		        for (c = 'A'; c <= 'Z'; ++c)
+			{
+				putchar(c);
+			}
+			for (c = 'a'; c <= 'z'; ++c)
+			{
+				putchar(c);    
+			}
+			return 0;
+}
 
 
