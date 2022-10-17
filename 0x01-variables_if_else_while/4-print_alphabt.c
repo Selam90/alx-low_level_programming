4e579653013ae3b27474c450a6e4432f584a21eb
Selam90 <selamnega0@gmail.com>
2022-10-17 05:15:58 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,10 @@ int main(void)
 	while 
 		(c <= 'z')
 		{
-			/* skip letter q and e
-			 */
 			if (c != 'q' && c != 'e')
 				putchar(c);
 			c++;
 		}
-	putchar("\n");
+	putchar('\n');
 			return (0);
 		}
