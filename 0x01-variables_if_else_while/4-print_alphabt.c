88b1c3520d1bffb9bfe72cd1dbd02cd5b1d29d88
Selam90 <selamnega0@gmail.com>
2022-10-17 04:31:28 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main - Entry 
+ * Return - 0
+ */
+int main(void)
+{
+	char c = 'a';
+	while 
+		(c <= 'z')
+		{
+			if(c != 'q' && c != 'e')
+				putchar(c);
+			c++;
+		}
+			return (0);
+		}
