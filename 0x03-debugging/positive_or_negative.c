6b33f89a514f59ea162619b5f91533261583d7f8
Selam90 <selamnega0@gmail.com>
2022-10-21 03:54:43 -0700

    .

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 /* more headers goes there */
 
@@ -9,6 +10,14 @@ int main(void)
 
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
-					/* your code goes there */
+				if (n > 0)
+					printf("%d is postive\n", n);
+				if (n == 0)
+					printf("%d is zero \n", n);
+				if (n < 0)
+				{
+					printf("%d is negative\n", n);
+				}			
 					return (0);
 }
+
