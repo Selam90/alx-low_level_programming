a204e45ee522ce37a226658d9dced0d376190d37
Selam90 <selamnega0@gmail.com>
2022-10-25 07:03:49 -0700

    .

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/** 
+ * print array - prints
+ * @a: array
+ * @n: num
+ */
+void print_array(int *a, int n)
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+	{
+		printf("%d", a[t]);
+		if (t != n -1)
+			printf(", ");
+	}
+	printf("\n ");
+}
