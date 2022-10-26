fa72115fb3a26891bfac2314950a3fc3d31f216f
Selam90 <selamnega0@gmail.com>
2022-10-26 06:03:47 -0700

    .

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,11 +7,21 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, dest_len = 0;
+	int i = 0;
+        int j;
 
-	while (dest[i++])
-		dest_len++;
-	for (index = 0; src[i]; i++)
-		dest[dest_len++] = src[i];
+	while (dest[i] != '\0' )
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+		dest[i] = src[j];
+
 	return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - compare
+ * @s1: string 1
+ * @s2: str 2
+ * Return: s1 and s2 difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
