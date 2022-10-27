64f75e1f78e24a8c1b1ba817183c4a7b328a6318
Selam90 <selamnega0@gmail.com>
2022-10-27 01:20:38 -0700

    .

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,21 @@
 #include "main.h"
 /**
- *_strncat - concatenates strings
+ *_strcat - concatenates strings
  * @src: source string
  * @dest: a pointer to the resulting string
- * @n: number
  * Return: A pointer to string @dest
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
-	int i,
-	int j;
+	int len = 0, i;
 
-	i = 0;
-	while (dest[i] != '\0')
+	while (dest[len])
+		len++;
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		i++;
+		dest[len] = src[i];
+		len += 1;
 	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-		dest[i] = '\0';
-		return (dest);
+	dest[len] = '\0';
+	return (dest);
 }
