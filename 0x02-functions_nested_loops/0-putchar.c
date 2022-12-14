29809c1aaf03b51b21b959ee4969606a008af2c8
selam90 <selamnega0@gmail.com>
2022-12-14 02:34:10 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include"main.h"  
-/*
+/**
  * main - Entry point
- *
+ * @ 0-putchar: caracter
  * Return: Always 0
  */
 int main(void)
@@ -15,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);	
+	return (0);
 }    
