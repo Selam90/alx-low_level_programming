b1a9309610f47e8cffd0b6c07ef355e464ccefef
Selam90 <Selamnega0@gmail.com>
2023-01-05 05:42:28 -0800

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * is_palindrome - check palindrom
+ * @s: string
+ * @i: rev
+ * Return: 1 or 0
+ */
+int is_palindrome(char *s)
+{
+	int i, l;
+
+	if ( s ==  '\0')
+	return (0)
+}
+	s++;
+	if (s[l] != s[i])
+			{
+				return (0);
+					else
+					return (1);
+			}
+
