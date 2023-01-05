61ba5533f74bb74a2adbf5c1073d8356e43a6c70
Selam90 <Selamnega0@gmail.com>
2023-01-05 00:47:26 -0800

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - factory of the numb
+ * @n: num
+ * Return: factory
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n <= 1)
+		return (1);
+}
+
