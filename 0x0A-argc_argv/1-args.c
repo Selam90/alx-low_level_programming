a2288094a314781de5bb60f81bc6d9a7fe1c3308
Selam90 <Selamnega0@gmail.com>
2023-01-10 00:55:50 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,10 @@
-#include <stido.h>
+#include <stdio.h>
 /**
  * @argc: argc
  * @argv: 
  * Return: 0 
  */
-int main(int arc, char *argv[] __attribute__((unused)))
+int main(int argc __attribute__((unused)),char *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
