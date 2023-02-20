88a2e5764634c53fe02796c1380c89d4acad1475
Selam90 <Selamnega0@gmail.com>
2023-02-20 00:16:31 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,12 @@
-#include<stdio.h>
-/*
- * main -display alpa
- * return 0
- */
+#include <stdio.h>                                                                                            /** 
+														*main                                                                                                                             
+ *                                                                                                                                    * return                                                                                                                           
+ *                                                                                                                                     * */                                                                                                                               
 int main(void)
 {
-	char a='a';
-	for (a='a'; a<='z';a++);
-	{
-		putchar(a);
-	}
-	putchar('\n');
-	return 0;
-}
+	char c;      
+
+	for (c = 'a'; c <= 'z'; c++)
+       	{
+		putchar(c);
+	} 
